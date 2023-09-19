7957f2d878cb4b48b92636b55ca2f90b2cce5aea
ARCHIT <145273087+Archiitt@users.noreply.github.com>
2023-09-19 11:45:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,34 @@ let isSpeaking = true;
 
 // Calls the function 'voices' to populate voice options
 voices();
+
+// Function to populate voice options
+function voices() {
+    // Iterates through the available voices from the Speech Synthesis API
+    for (let voice of synth.getVoices()) {
+        // Checks if the voice name matches "Google US English" and sets 'selected' attribute accordingly
+        let selected = voice.name === "Google US English" ? "selected" : "";
+
+        // Generates an HTML option element with voice name and language
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+
+        // Inserts the generated option into the 'voiceList' (select element) in the DOM
+        voiceList.insertAdjacentHTML("beforeend", option);
+    }
+}
+
+// Event listener that listens for changes in available voices and calls the 'voices' function when voices change
+synth.addEventListener("voiceschanged", voices);
+
+// Function to convert text to speech
+function textToSpeech(text) {
+    // Creates a new SpeechSynthesisUtterance object with the specified text
+    let utterance = new SpeechSynthesisUtterance(text);
+
+    // Iterates through available voices to find the selected voice from the voiceList
+    for (let voice of synth.getVoices()) {
+        if (voice.name === voiceList.value) {
+            // Sets the selected voice for the utterance
+            utterance.voice = voice;
+        }
+    }
